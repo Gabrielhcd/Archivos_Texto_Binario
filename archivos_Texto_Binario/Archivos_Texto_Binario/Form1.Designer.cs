b8e66dff4ef564aa9b083ead0d42f2dc5118e24b
VanHoemhein <ghcarrasco@hotmail.com>
2018-02-28 21:47:53 -0600

    Second commit

---
+++ archivos_Texto_Binario/Archivos_Texto_Binario/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Archivos_Texto_Binario
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.oFD1 = new System.Windows.Forms.OpenFileDialog();
+			this.sFD1 = new System.Windows.Forms.SaveFileDialog();
+			this.btnselecBMP = new System.Windows.Forms.Button();
+			this.txtCaractBMP = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// oFD1
+			// 
+			this.oFD1.FileName = "openFileDialog1";
+			// 
+			// btnselecBMP
+			// 
+			this.btnselecBMP.Location = new System.Drawing.Point(12, 12);
+			this.btnselecBMP.Name = "btnselecBMP";
+			this.btnselecBMP.Size = new System.Drawing.Size(106, 56);
+			this.btnselecBMP.TabIndex = 0;
+			this.btnselecBMP.Text = "Seleccionar BMP";
+			this.btnselecBMP.UseVisualStyleBackColor = true;
+			this.btnselecBMP.Click += new System.EventHandler(this.btnselecBMP_Click);
+			// 
+			// txtCaractBMP
+			// 
+			this.txtCaractBMP.Location = new System.Drawing.Point(145, 12);
+			this.txtCaractBMP.Multiline = true;
+			this.txtCaractBMP.Name = "txtCaractBMP";
+			this.txtCaractBMP.Size = new System.Drawing.Size(188, 161);
+			this.txtCaractBMP.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(921, 485);
+			this.Controls.Add(this.txtCaractBMP);
+			this.Controls.Add(this.btnselecBMP);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog oFD1;
+		private System.Windows.Forms.SaveFileDialog sFD1;
+		private System.Windows.Forms.Button btnselecBMP;
+		private System.Windows.Forms.TextBox txtCaractBMP;
+	}
+}
+
---
+++ archivos_Texto_Binario/Archivos_Texto_Binario/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Archivos_Texto_Binario
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Abre ventana de busqueda de archivos y almacena la extension
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnselecBMP_Click(object sender, EventArgs e)
+		{
+			oFD1.ShowDialog();
+			FileStream file = new FileStream(oFD1.FileName, FileMode.Open);
+			StreamReader sr = new StreamReader(file);
+			Bitmap img = new Bitmap(oFD1.FileName);
+			string filext = Path.GetExtension(oFD1.FileName);
+			filesize(img);
+			validar(filext);
+			file.Close();
+			sr.Close();
+		}
+
+		/// <summary>
+		/// Valida que la extension del archivo sea la correcta, de lo contrario muestra un
+		/// mensaje de error
+		/// </summary>
+		/// <param name="exte"></param>
+		private void validar(string exte)
+		{
+			string vldr = System.IO.Path.GetExtension(exte);
+			if (vldr != ".bmp")
+			{
+				MessageBox.Show("¡FORMATO INCORRECTO!");
+			}
+		}
+		
+		private void filesize(Bitmap img)
+		{
+			
+			var imgWidth = img.Width; //Ancho en pixeles 
+			var imgHeight = img.Height; //Alto en pixeles
+			byte tamano = Convert.ToByte(imgHeight * imgWidth); //tamaño en bytes
+			txtCaractBMP.Text = "Ancho: " + Convert.ToString(imgWidth) + "Alto: " + Convert.ToString(imgHeight) + "Tamaño: " +
+				Convert.ToString(tamano);
+		}
+	}
+}
